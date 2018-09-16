d3ac50c0c05507cc5cfe79b68ae9c389b5f699b3
Yilei He <yileihe1989@hotmail.com>
2018-09-16 20:15:30 +0100

    Add txt and py script

---
+++ Start_Review.py
@@ -0,0 +1,73 @@
+import random
+import textwrap
+
+
+def word_preprocess(item):
+    if len(item) <= 1:
+        raise Exception("Invalid word item: {}".format(item))
+
+    item_clean = [i for i in item if i != ""] if len(item) > 2 else item
+
+    if len(item_clean) > 2:
+        raise Exception("Invalid word item: {}".format(item_clean))
+
+    item_clean[-1] = int(item_clean[-1])
+
+    return item_clean
+
+
+def read_data(path):
+    freader = open(path, "r")
+    words_collection = list()
+    for line in freader:
+        item = line.strip().split("\t")
+        item_clean = word_preprocess(item)
+
+        words_collection.append(item_clean)
+
+    freader.close()
+    return sorted(words_collection, key=lambda x: x[1])
+
+
+def bs_find_index(array,value):
+    length = len(array)
+    start = 0
+    end = length - 1
+    while (end-start)>1:
+        mid = start + (end - start)//2
+        if array[mid][1] > value:
+            end = mid
+        else:
+            start = mid
+    return start
+
+
+def execute_learning(words_collection,end_point):
+    ifcont = "Y"
+    reviewed = set()
+    while ifcont in ['Y', 'y', "", "yes"]:
+        curr_index = random.randint(0,end_index)
+        if curr_index not in reviewed:
+            print (curr_index)
+            print (textwrap.fill(words_collection[curr_index][0]))
+            words_collection[curr_index][1] += 1
+            reviewed.add(curr_index)
+            ifcont = input("If Continue?")
+        else:
+            continue
+
+    print("Total sentences reviewed today is {}".format(len(reviewed)))
+
+
+if __name__ == "__main__":
+    file_path = "./words_collection.txt"
+    words_collection = read_data(file_path)
+    count_treshold = int(input("Less than?"))
+    end_index = bs_find_index(words_collection, count_treshold)
+
+    execute_learning(words_collection, end_index)
+
+    fwrite = open(file_path, "w")
+    for item in words_collection:
+        fwrite.write("\t".join([str(i) for i in item]) + "\n")
+    fwrite.close()
